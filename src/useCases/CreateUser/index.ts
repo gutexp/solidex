13e86f07d001498dc3d8b0662cee170d197f806a
gutexp <gutexp@hotmail.com>
2021-02-17 15:19:52 -0300

    changes
    testing changes

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,19 @@
+import { MailtrapMailProvider } from "../../providers/implementations/MailtrapMailProvider";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserController } from "./CreateUserController";
+import { PostgresUsersRepository } from "../../repositories/implementations/PostgresUsersRepositories";
+
+const postgresUsersRepository = new PostgresUsersRepository
+const mailtrapMailProvider = new MailtrapMailProvider()
+
+const createUserUseCase = new CreateUserUseCase(
+  postgresUsersRepository,
+  mailtrapMailProvider,
+)
+
+const createUserController = new CreateUserController(
+  createUserUseCase
+)
+
+export { createUserUseCase, createUserController }
